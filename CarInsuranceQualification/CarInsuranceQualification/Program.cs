f29d72e3f3a139fb111cd9961ab4d3b5187ec508
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-01 10:43:25 +0100

    Add files via upload

---
+++ CarInsuranceQualification/CarInsuranceQualification/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarInsuranceQualification
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Lets check if you are qualified for a car insurance");
+            Console.WriteLine("What is your age?");
+            int ageInt = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Have you ever had a DUI? Reply true or false");
+            string everDUI = Console.ReadLine();
+            bool EverDuiBool = Boolean.Parse(everDUI);
+
+            Console.WriteLine("How many speeding tickets do you have?");
+            int speedingTicketInt = Convert.ToInt32(Console.ReadLine());
+
+            bool isQualified = ageInt >= 15 && EverDuiBool == false && speedingTicketInt <= 3;
+
+            Console.WriteLine("The person you have had entered their details is qualified for DUI?: " + isQualified);
+            Console.ReadLine();
+        }
+    }
+}
