b173016d8be075a70a2bf62f7110985691dbb185
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-02 08:03:01 +0100

    Add files via upload

---
+++ WhileLoop/WhileLoop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhileLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            int i = 0;
+            while (i < daysOfWeek.Length)
+            {
+                Console.WriteLine(daysOfWeek[i]);
+                i++;
+            }
+
+            int ii = 1;
+
+            do
+            {
+                Console.WriteLine(ii);
+                ii++;
+            } while (ii <= 5);
+
+            Console.ReadLine();
+        }
+    }
+
+}
+
