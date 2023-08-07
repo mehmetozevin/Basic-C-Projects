bd5f7ba648e2077d40014215c9186cedbae6ec7f
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-07 10:21:16 +0100

    Add files via upload

---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Person.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    public class Person
+    {
+        // Properties
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        // Void method to say the person's name
+        public void SayName()
+        {
+            Console.WriteLine($"Name: {FirstName} {LastName}");
+        }
+    }
+}
---
+++ MethodsAndObjectsAssignment/MethodsAndObjectsAssignment/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAndObjectsAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate and initialize an Employee object
+            Employee employee = new Employee
+            {
+                FirstName = "Sample",
+                LastName = "Student",
+                Id = 123
+            };
+
+            // Call the superclass method SayName() on the Employee object
+            employee.SayName();
+
+            // Wait for the user to press a key before closing the console window
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
