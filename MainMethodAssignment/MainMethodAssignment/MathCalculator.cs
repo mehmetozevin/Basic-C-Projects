13711d82f105d6113ef04feee11dec2831a8e81c
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-07 08:20:42 +0100

    Add files via upload

---
+++ MainMethodAssignment/MainMethodAssignment/MathCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    public class MathCalculator
+    {
+        // Method to perform math operation on an integer
+        public int PerformOperation(int num)
+        {
+            return num + 10; // Example operation: addition
+        }
+
+        // Method to perform math operation on a decimal
+        public int PerformOperation(decimal num)
+        {
+            return (int)(num * 2); // Example operation: multiplication
+        }
+
+        // Method to perform math operation on a string (if it's a valid integer)
+        public int PerformOperation(string input)
+        {
+            if (int.TryParse(input, out int num))
+            {
+                return num - 5; // Example operation: subtraction
+            }
+            else
+            {
+                return 0; // Invalid input, return 0
+            }
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathCalculator class
+            MathCalculator calculator = new MathCalculator();
+
+            // Call the method for integer and display the result
+            int resultInt = calculator.PerformOperation(15);
+            Console.WriteLine($"Result for integer: {resultInt}");
+
+            // Call the method for decimal and display the result
+            decimal inputDecimal = 3.5M;
+            int resultDecimal = calculator.PerformOperation(inputDecimal);
+            Console.WriteLine($"Result for decimal: {resultDecimal}");
+
+            // Call the method for string and display the result
+            string inputString = "10";
+            int resultString = calculator.PerformOperation(inputString);
+            Console.WriteLine($"Result for string: {resultString}");
+
+            // Wait for the user to press a key before closing the console window
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
