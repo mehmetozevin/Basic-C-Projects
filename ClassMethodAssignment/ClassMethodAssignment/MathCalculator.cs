2a202ee432ac047ae6b106ef72e8d286ed03ec4c
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-07 09:05:40 +0100

    Add files via upload

---
+++ ClassMethodAssignment/ClassMethodAssignment/MathCalculator.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    public class MathCalculator
+    {
+        // Void method that divides the data passed to it by 2
+        public void DivideByTwo(int num)
+        {
+            int result = num / 2;
+            Console.WriteLine($"Result after dividing {num} by 2: {result}");
+        }
+
+        // Method with output parameters
+        public void CalculateSumAndDifference(int num1, int num2, out int sum, out int difference)
+        {
+            sum = num1 + num2;
+            difference = num1 - num2;
+        }
+
+        // Overloaded method
+        public void CalculateSumAndDifference(double num1, double num2, out double sum, out double difference)
+        {
+            sum = num1 + num2;
+            difference = num1 - num2;
+        }
+    }
+}
---
+++ ClassMethodAssignment/ClassMethodAssignment/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathCalculator class
+            MathCalculator calculator = new MathCalculator();
+
+            // Ask the user to enter a number
+            Console.Write("Enter a number: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            // Call the method on the entered number
+            calculator.DivideByTwo(userInput);
+
+            // Using method with output parameters
+            int num1 = 10, num2 = 5;
+            calculator.CalculateSumAndDifference(num1, num2, out int sum, out int difference);
+            Console.WriteLine($"Sum of {num1} and {num2}: {sum}");
+            Console.WriteLine($"Difference of {num1} and {num2}: {difference}");
+
+            // Using overloaded method with double parameters
+            double doubleNum1 = 7.5, doubleNum2 = 3.2;
+            calculator.CalculateSumAndDifference(doubleNum1, doubleNum2, out double doubleSum, out double doubleDifference);
+            Console.WriteLine($"Sum of {doubleNum1} and {doubleNum2}: {doubleSum}");
+            Console.WriteLine($"Difference of {doubleNum1} and {doubleNum2}: {doubleDifference}");
+
+            // Wait for the user to press a key before closing the console window
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
