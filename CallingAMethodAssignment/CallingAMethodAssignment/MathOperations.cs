d0b92947e0e30bb919a8de05354057e55149302b
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-07 07:38:25 +0100

    Add files via upload

---
+++ CallingAMethodAssignment/CallingAMethodAssignment/MathOperations.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingAMethodAssignment
+{
+    public class MathOperations
+    {
+        // Method to perform addition
+        public int Add(int num)
+        {
+            return num + 10;
+        }
+
+        // Method to perform subtraction
+        public int Subtract(int num)
+        {
+            return num - 5;
+        }
+
+        // Method to perform multiplication
+        public int Multiply(int num)
+        {
+            return num * 2;
+        }
+    }
+}
---
+++ CallingAMethodAssignment/CallingAMethodAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingAMethodAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ask the user what number they want to do the math operations on
+            Console.Write("Enter a number for math operations: ");
+            int userInput = int.Parse(Console.ReadLine());
+
+            // Create an instance of the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call each method and display the returned integer
+            int resultAdd = mathOps.Add(userInput);
+            Console.WriteLine("Result of addition 10 to you number: {resultAdd}");
+
+            int resultSubtract = mathOps.Subtract(userInput);
+            Console.WriteLine($"Result of subtraction 5 from your selected number: {resultSubtract}");
+
+            int resultMultiply = mathOps.Multiply(userInput);
+            Console.WriteLine($"Result of multiplication with 2: {resultMultiply}");
+
+            // Wait for the user to press a key before closing the console window
+            Console.WriteLine("Press to exit.");
+            Console.ReadKey();
+        }
+    }
+}
