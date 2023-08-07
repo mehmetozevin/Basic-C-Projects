23434267b25d6945829afd3ba4e62f2ba1d3bf54
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-07 08:45:14 +0100

    Add files via upload

---
+++ SubmitMethod/SubmitMethod/MathOperations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubmitMethod
+{
+    public class MathOperations
+    {
+        // Void method that performs a math operation on the first integer
+        // and displays the second integer to the screen
+        public void PerformAndDisplay(int num1, int num2)
+        {
+            int result = num1 * 2; // Example operation: multiplication
+            Console.WriteLine($"Result of the operation on {num1}: {result}");
+            Console.WriteLine($"Second integer: {num2}");
+        }
+    }
+}
---
+++ SubmitMethod/SubmitMethod/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubmitMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Instantiate the MathOperations class
+            MathOperations mathOps = new MathOperations();
+
+            // Call the method in the class, passing in two numbers
+            mathOps.PerformAndDisplay(5, 10);
+
+            // Call the method in the class, specifying the parameters by name
+            mathOps.PerformAndDisplay(num2: 20, num1: 3);
+
+            // Wait for the user to press a key before closing the console window
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
