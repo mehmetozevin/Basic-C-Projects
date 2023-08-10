04be2acc7121fb5315dbc414e074b55f2239e953
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-10 08:34:20 +0100

    Add files via upload

---
+++ InputAssignment/InputAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            // Prompt the user for a number
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+
+            // Log the number to a text file
+            using (StreamWriter writer = new StreamWriter("number_log.txt", true))
+            {
+                writer.WriteLine(userInput);
+            }
+
+            // Read and print the text file content back to the user
+            using (StreamReader reader = new StreamReader("number_log.txt"))
+            {
+                string fileContent = reader.ReadToEnd();
+                Console.WriteLine("Numbers logged in the file:");
+                Console.WriteLine(fileContent);
+            }
+        }
+        catch (Exception ex)
+        {
+            // Handle any exceptions that might occur
+            Console.WriteLine($"An error occurred: {ex.Message}");
+        }
+    }
+}
