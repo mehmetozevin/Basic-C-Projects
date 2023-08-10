a48a67674615b43fbbdc332fc03572507f9bc340
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-10 09:01:31 +0100

    Add files via upload

---
+++ Datetime Assignment/Datetime Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Datetime_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                // Print the current date and time
+                DateTime currentDateTime = DateTime.Now;
+                Console.WriteLine($"Current Date and Time: {currentDateTime}");
+
+                // Prompt the user for a number
+                Console.Write("Enter a number of hours: ");
+                int hoursToAdd = Convert.ToInt32(Console.ReadLine());
+
+                // Calculate the future date and time
+                DateTime futureDateTime = currentDateTime.AddHours(hoursToAdd);
+                Console.WriteLine($"Exact time in {hoursToAdd} hours: {futureDateTime}");
+            }
+            catch (Exception ex)
+            {
+                // Handle any exceptions that might occur
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+    }
+}
