d1bb3c5dd958da275b6d1eb6f66d7628d52df85d
mehmetozevin <122027910+mehmetozevin@users.noreply.github.com>
2023-08-18 12:20:25 +0300

    Add files via upload

---
+++ BStudentDB/BStudentDB/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BStudentDB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new StudentDbContext())
+            {
+                // Create a new student
+                var newStudent = new Student
+                {
+                    FirstName = "John",
+                    LastName = "Doe"
+                };
+
+                // Add the student to the database
+                context.Students.Add(newStudent);
+                context.SaveChanges();
+
+                Console.WriteLine("Student added to the database.");
+            }
+        }
+    }
+}
---
+++ BStudentDB/BStudentDB/StudentDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+
+
+namespace BStudentDB
+{
+
+    public class StudentDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+    }
+}
